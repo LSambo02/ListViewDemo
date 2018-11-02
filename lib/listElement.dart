5916e739cf2416a6dd6f2709064a143a65415fc8
igorsambo <igorlsambo1999@icloud.com>
2018-11-02 10:17:31 +0200

    primeiro commit

--- lib/listElement.dart
+++ lib/listElement.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/material.dart';
+import 'detalhes.dart';
 
 class Renderizacao extends StatelessWidget {
-  List<String> data =[];
-  Renderizacao(List data);
+  List<String> data = [];
 
-
+  Renderizacao(this.data);
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    print(data.length);
     return new Expanded(
         child: new ListView.builder(
             itemCount: data.length,
-            itemBuilder: (BuildContext bcontext, int index) {
-              return new Row(
-                children: <Widget>[
-                  Image.asset('assets/person.jpg',height: 200.0,width: 150.0,),
-                  Text(data[index])
-                ],
+            itemBuilder: (BuildContext context, int index) {
+              return new RaisedButton(
+                onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return DetailPage('assets/flutter.png', data[index]);
+                    }));
+
+                },
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/flutter.png',
+                      height: 90.0,
+                      width: 75.0,
+                    ),
+                    Text(data[index])
+                  ],
+                ),
               );
             }));
   }
